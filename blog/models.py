42268cfdf3e9c075bfcef5cb011ce3258b548c04
gokul-github-collab <gokulakrishnan0604@gmail.com>
2023-07-08 18:42:32 +0530

    Added Models to Blog Site

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
+from typing import Any
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.caption
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excert = models.CharField(max_length=300)
+    image_name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(null=True, unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
